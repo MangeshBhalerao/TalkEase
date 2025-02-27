77ba0240e7f19837202e9c50ccf4bae5abe80791
Mangesh <157815072+MangeshBhalerao@users.noreply.github.com>
2025-02-27 21:43:27 +0530

    Delete Backend/config.py

--- Backend/config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.urandom(24)
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:Mangesh@127.0.0.1:5432/postgres'
